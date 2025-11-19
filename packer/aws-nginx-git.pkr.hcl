6a6ca8472d0ef3b016b3a43f94203520ccde7622
ahmedwoye@gmail.com Woye1234@@ <ahmedwoye@gmail.com>
2025-11-19 17:06:46 +0000

    Server updated with the terraform file and READ.ME

--- packer/aws-nginx-git.pkr.hcl
+++ packer/aws-nginx-git.pkr.hcl
@@ -21,6 +21,12 @@ source "amazon-ebs" "nginx-git" {
     source_ami  = "ami-0870af38096a5355b"
     ami_name = "nginx-git-by-packer-v2"
     ami_virtualization_type  = "hvm"
+
+    tags = {
+    Name        = "nginx-git-ami"
+    Environment = "production"
+    Project     = "nginx"
+  }
 }
 
 
@@ -35,7 +41,35 @@ source "amazon-ebs" "java-git" {
     source_ami  = "ami-0870af38096a5355b"
     ami_name = "java-git-by-packer-v2"
     ami_virtualization_type  = "hvm"
+
+    tags = {
+    Name        = "java-git-ami"
+    Environment = "production"
+    Project     = "Java"
+  }
 }
+
+
+#-----------------------------
+# source: how we build the AMI For Nginx and GIT 
+#-----------------------------
+
+source "amazon-ebs" "python-git" {
+    region = "eu-west-1"
+    instance_type = "t3.micro"
+    ssh_username = "ec2-user"
+    source_ami  = "ami-0870af38096a5355b"
+    ami_name = "python-git-by-packer-v2"
+    ami_virtualization_type  = "hvm"
+
+    tags = {
+    Name        = "python-git-ami"
+    Environment = "production"
+    Project     = "Python"
+  }
+}
+
+
 
 
 #------------------------------------
@@ -74,13 +108,42 @@ build  {
     provisioner "shell" {
         inline = [
             "sudo yum update -y",
+            "sudo yum install nginx -y",
+            "sudo systemctl enable nginx",
+            "sudo systemctl start nginx",
+            "echo  '<h1> Hello from Techbleat - Java Node Built by Packer </h1>' | sudo tee /usr/share/nginx/html/index.html",
             "sudo yum install java-17-amazon-corretto -y",
+            "sudo yum install maven -y",
             "sudo yum install git -y"
         ]
     }
 
     post-processor "shell-local" {
         inline = ["echo 'AMI build is finished For Java' "]
+    }
+
+}
+
+
+build  {
+    name  = "python-git-ami-build"
+    sources = [
+        "source.amazon-ebs.python-git"
+    ]
+
+    provisioner "shell" {
+        inline = [
+            "sudo yum update -y",
+            "sudo yum install nginx -y",
+            "sudo systemctl enable nginx",
+            "sudo systemctl start nginx",
+            "echo  '<h1> Hello from Techbleat - Python Node Built by Packer </h1>' | sudo tee /usr/share/nginx/html/index.html",
+            "sudo yum install git -y"
+        ]
+    }
+
+    post-processor "shell-local" {
+        inline = ["echo 'AMI build is finished For Python' "]
     }
 
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,169 @@
-#place holder for Terraform
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+
+
+# ============================
+# nginx Instance (Standalone)
+# ============================
+resource "aws_instance" "nginx-node" {
+  ami                    = "ami-0cec7e1189d500542"
+  instance_type          = "t3.micro"
+  subnet_id              = "subnet-0828482fafcb40dc8"
+  vpc_security_group_ids = [aws_security_group.nginx_access.id]
+  key_name               = "October2025"
+
+  tags = {
+    Name = "terraform-nginx-node"
+  }
+}
+
+
+
+# ============================
+# Security Group for nginx
+# ============================
+resource "aws_security_group" "nginx_access" {
+  name        = "allow_ssh_http_nginx"
+  description = "Allow SSH and HTTP inbound traffic"
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP on port 8080"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ============================
+# Security Group for Java
+# ============================
+resource "aws_security_group" "java_access" {
+  name        = "allow_ssh_http_java"
+  description = "Allow SSH and HTTP inbound traffic"
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ============================
+# Java Instance (Standalone)
+# ============================
+resource "aws_instance" "java-node" {
+  ami                    = "ami-09c1b59b70df408bc"
+  instance_type          = "t3.micro"
+  subnet_id              = "subnet-0828482fafcb40dc8"
+  vpc_security_group_ids = [aws_security_group.java_access.id]
+  key_name               = "October2025"
+
+  tags = {
+    Name = "terraform-java-node"
+  }
+}
+
+
+# ============================
+# Security Group for Python
+# ============================
+resource "aws_security_group" "python_access" {
+  name        = "allow_ssh_http_nginx"
+  description = "Allow SSH and HTTP inbound traffic"
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP on port 8080"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+# ============================
+# Python Instance (Standalone)
+# ============================
+resource "aws_instance" "python-node" {
+  ami                    = "ami-0cec7e1189d500542"
+  instance_type          = "t3.micro"
+  subnet_id              = "subnet-0828482fafcb40dc8"
+  vpc_security_group_ids = [aws_security_group.python_access.id]
+  key_name               = "October2025"
+
+  tags = {
+    Name = "terraform-python-node"
+  }
+}
